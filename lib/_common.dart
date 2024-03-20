42fa1200bdb7de6f1dfc208663bb4449a988228f
robmllze <86869208+robmllze@users.noreply.github.com>
2024-03-20 14:32:33 +1100

    Changed to double quotes as default

--- lib/_common.dart
+++ lib/_common.dart
@@ -10,15 +10,15 @@
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 //.title~
 
-export 'dart:async';
+export "dart:async";
 
-export 'package:meta/meta.dart';
+export "package:meta/meta.dart";
 
-export 'package:xyz_gen_annotations/xyz_gen_annotations.dart';
-export 'package:xyz_pod/xyz_pod.dart';
-export 'package:xyz_utils/xyz_utils.dart';
+export "package:xyz_gen_annotations/xyz_gen_annotations.dart";
+export "package:xyz_pod/xyz_pod.dart";
+export "package:xyz_utils/xyz_utils.dart";
 
-export 'package:_data/lib.dart';
-export 'package:_service_interfaces/lib.dart';
+export "package:_data/lib.dart";
+export "package:_service_interfaces/lib.dart";
 
-export 'src/_all_src.g.dart';
+export "src/_all_src.g.dart";
