dfe756bf85f6e340558aa7a2c6fea311cc9e11cb
robmllze <86869208+robmllze@users.noreply.github.com>
2024-04-15 19:49:24 +1000

    Update

--- lib/src/service_utils/job_utils.dart
+++ lib/src/service_utils/job_utils.dart
@@ -12,28 +12,20 @@ import '/_common.dart';
 
 // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
-final class OrganizationUtils {
+final class JobUtils {
   //
   //
   //
 
-  OrganizationUtils._();
-
-  //
-  //
-  //
-
-  static Iterable<ModelOrganizationPub> filterOrganizationPubsByRelationship({
+  static Iterable<ModelJobPub> filterJobPubsByRelationship({
     required String? relationshipId,
     required Iterable<ModelRelationship>? relationshipPool,
-    required Iterable<ModelOrganizationPub>? organizationPubPool,
+    required Iterable<ModelJobPub>? jobPubPool,
   }) {
     final relationship = ModelRelationship.fromPool(pool: relationshipPool, id: relationshipId);
     if (relationship != null) {
-      final pids = RelationshipUtils.extractOrganizationMemberPids(relationship: relationship);
-      final result = pids
-          .map((pid) => ModelOrganizationPub.fromPool(pool: organizationPubPool, id: pid))
-          .nonNulls;
+      final pids = RelationshipUtils.extractJobMemberPids(relationship: relationship);
+      final result = pids.map((pid) => ModelJobPub.fromPool(pool: jobPubPool, id: pid)).nonNulls;
       return result;
     } else {
       return [];
--- lib/src/service_utils/project_utils.dart
+++ lib/src/service_utils/project_utils.dart
@@ -12,28 +12,21 @@ import '/_common.dart';
 
 // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
-final class OrganizationUtils {
+final class ProjectUtils {
   //
   //
   //
 
-  OrganizationUtils._();
-
-  //
-  //
-  //
-
-  static Iterable<ModelOrganizationPub> filterOrganizationPubsByRelationship({
+  static Iterable<ModelProjectPub> filterProjectPubsByRelationship({
     required String? relationshipId,
     required Iterable<ModelRelationship>? relationshipPool,
-    required Iterable<ModelOrganizationPub>? organizationPubPool,
+    required Iterable<ModelProjectPub>? projectPubPool,
   }) {
     final relationship = ModelRelationship.fromPool(pool: relationshipPool, id: relationshipId);
     if (relationship != null) {
-      final pids = RelationshipUtils.extractOrganizationMemberPids(relationship: relationship);
-      final result = pids
-          .map((pid) => ModelOrganizationPub.fromPool(pool: organizationPubPool, id: pid))
-          .nonNulls;
+      final pids = RelationshipUtils.extractProjectMemberPids(relationship: relationship);
+      final result =
+          pids.map((pid) => ModelProjectPub.fromPool(pool: projectPubPool, id: pid)).nonNulls;
       return result;
     } else {
       return [];
