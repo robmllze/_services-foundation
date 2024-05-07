edfb35219ca3fdaef28457ccbd463cabb2c53163
robmllze <86869208+robmllze@users.noreply.github.com>
2024-05-07 21:42:23 +1000

    Update

--- lib/src/service_utils/file_utils.dart
+++ lib/src/service_utils/file_utils.dart
@@ -8,7 +8,7 @@
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 //.title~
 
-//import '/_common.dart';
+import '/_common.dart';
 
 // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
@@ -18,4 +18,21 @@ final class FileUtils {
   //
 
   FileUtils._();
+
+  //
+  //
+  //
+
+  static Future<void> deleteCurrentAvatarFromBackend({
+    required ServiceEnvironment serviceEnvironment,
+    required Iterable<ModelFileEntry> filesSnapshots,
+  }) async {
+    if (filesSnapshots.isEmpty) return;
+    final avatarSnapshots = filesSnapshots.where(
+      (e) => e.isFlutterImageExtension() && e.definitionPathStartsWith(FileSchema.AVATAR),
+    );
+    final refs = avatarSnapshots.map((e) => e.id).nonNulls.map((e) => Schema.fileRef(fileId: e));
+    final deletes = refs.map((e) => serviceEnvironment.fileServiceBroker.deleteFile(e));
+    await Future.wait(deletes);
+  }
 }
